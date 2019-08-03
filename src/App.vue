cb0c0422ed64d473e62b03b36a561475377b14a0
Axel Dürkop <axel.duerkop@tu-harburg.de>
2019-08-04 01:48:33 +0200

    Add transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Navigation />
-    <router-view></router-view>
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
     <Footer />
   </div>
 </template>
@@ -20,38 +22,10 @@ export default {
     Footer
   },
   methods: {
-    ...mapActions(["set_groupID", "set_list_collections", "create"]),
-    parse_query_string: function() {
-      // Credits to https://www.joezimjs.com/javascript/3-ways-to-parse-a-query-string-in-a-url/
-      let queryString = window.location.search.substring(1);
-      let params = {},
-        queries,
-        temp,
-        i,
-        l;
-      // Split into key/value pairs
-      queries = queryString.split("&");
-      // Convert the array of strings into an object
-      for (i = 0, l = queries.length; i < l; i++) {
-        temp = queries[i].split("=");
-        params[temp[0]] = temp[1];
-      }
-      return params;
-    }
+    ...mapActions(["set_groupID", "set_list_collections", "create"])
   },
   created() {
     window.console.log("App created()...");
-    window.console.log(this.parse_query_string());
-    let params = this.parse_query_string();
-    if ("groupID" in params) {
-      this.$store.commit("SET_LOADING_STATUS", "done");
-      window.console.log("Group ID found!");
-      this.set_groupID(params.groupID);
-    }
-    if ("list_collections" in params) {
-      window.console.log("I have to list all collections!");
-      this.set_list_collections(params.list_collections);
-    }
   }
 };
 </script>
