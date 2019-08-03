2b1ea8114bb07e842f4beaf64f7cd9f6908e92df
Axel Dürkop <axel.duerkop@tu-harburg.de>
2019-08-04 01:48:45 +0200

    Check params of list view

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
-import Vuex from 'vuex'
+import Vuex from "vuex";
 import App from "./App.vue";
 import AsyncComputed from "vue-async-computed";
 import VueDates from "vue-date-filters";
 import VueSimpleMarkdown from "vue-simple-markdown";
 import storeConfig from "./store/store-config";
-import router from "./router"
+import router from "./router";
 // import store from "./store";
 import "vue-simple-markdown/dist/vue-simple-markdown.css";
 import VModal from "vue-js-modal";
@@ -18,7 +18,12 @@ Vue.use(VueSimpleMarkdown);
 Vue.use(VModal, { dialog: true });
 Vue.use(Vuex);
 
-const store = new Vuex.Store(storeConfig);
+export const store = new Vuex.Store(storeConfig);
+
+// router.beforeEach((to, from, next) => {
+//   window.console.log("Before each route...");
+//   next();
+// });
 
 new Vue({
   store,
--- src/router.js
+++ src/router.js
@@ -4,8 +4,28 @@ import Home from "./components/Home.vue";
 import New from "./components/New.vue";
 import List from "./components/List.vue";
 import About from "./components/About.vue";
+import { store } from "./main";
 
 Vue.use(Router);
+
+// Return the query parameters if any
+function getQuery(route) {
+  let params = route.query;
+
+  window.console.log("Parameters: ", params);
+
+  if ("groupID" in params) {
+    window.console.log("Group ID found!");
+    window.console.log(store)
+    store.commit("SET_GROUPID", params.groupID)
+  }
+  if ("list_collections" in params) {
+    window.console.log("I have to list all collections!");
+    store.commit("SET_LIST_COLLECTION", true)
+  }
+
+  return route.query;
+}
 
 export default new Router({
   routes: [
@@ -27,7 +47,12 @@ export default new Router({
     {
       path: "/list",
       name: "list",
-      component: List
+      component: List,
+      props: getQuery,
+      beforeEnter: (to, from, next) => {
+        window.console.log("Checking parameters in list route...");
+        next();
+      }
     }
   ]
 });
