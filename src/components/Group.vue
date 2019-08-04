eeb5285f3e8079755000acb7ffd5f3648e4f7e38
Axel Dürkop <axel.duerkop@tu-harburg.de>
2019-08-04 17:57:27 +0200

    Add API calls depending on filter config

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
+  <div class="mt-4">
     <h3 class="section-header" v-show="meta_data.groupURL != ''">
       Zotero Group:
       <a :href="( meta_data.groupURL || '#')" target="_blank">{{ meta_data.name }}</a>
     </h3>
+
+    <!-- Multiple collections -->
     <div v-if="list_collections && !get_init">
       <div v-for="collection in collections" :key="collection.index">
         <Collection :title="collection.name" :collectionKey="collection.key" />
       </div>
     </div>
 
+    <!-- Single collection -->
+    <div v-if="list_collections && collectionKey != null && !get_init">
+      <Collection :title="collectionKey" :collectionKey="collectionKey" />
+    </div>
+
+    <!-- Complete library -->
     <div v-if="!list_collections">
       <Item v-for="item in items" :key="item.index" :item="item" />
     </div>
@@ -52,22 +60,37 @@ export default {
     ])
   },
   created() {
+
     // Get only one item to get the library title etc.
-    window.console.log("Inside Group created()...")
+    window.console.log("Inside Group created()...");
     this.fetch_complete_zotero_list(1).then(response => {
       window.console.log("Data for Library Meta: ", response.raw);
       if (response.raw.length == 1) {
         this.meta_data = {
           name: response.raw[0].library.name,
-          groupURL: response.raw[0].library.links.alternate.href + "/items"
+          groupURL: response.raw[0].library.links.alternate.href + "/items",
+          description: response.raw[0].library.description
         };
       } else {
         window.console.error("No items in this library!");
       }
     });
-    // This method get a collection key from the parent
-    //  component and fetches its items asynchronously
-    if (!this.list_collections) {
+
+// List all collections
+    if (this.list_collections && this.collectionKey === null) {
+      this.$store
+        .dispatch(
+          "fetch_top_level_collections",
+          this.groupID,
+          this.collectionKey
+        )
+        .then(() => {
+          this.$store.commit("SET_SEARCH_DONE", true);
+        });
+    }
+
+    // List the complete library if collection is false
+    else if (!this.list_collections) {
       this.fetch_complete_zotero_list().then(response => {
         this.map_items(response).then(response => {
           this.$store.commit("SET_LOADING_STATUS", "done");
