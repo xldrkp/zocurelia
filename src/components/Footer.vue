21a14ca301e3632077fe5db32f968d608e54f8fa
Axel Dürkop <axel.duerkop@tu-harburg.de>
2019-08-11 12:31:47 +0200

    Improve sticky footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container py-2">
+  <div id="footer" class="container py-2 fixed-bottom">
     <div class="row">
       <div class="col-md-12 text-center">
         <p class="mb-0">© 2019 Axel Dürkop. All rights reserved</p>
